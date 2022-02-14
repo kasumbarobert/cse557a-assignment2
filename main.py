43204ce5c66852866111513de825a76349002e3f
Kasumba Robert <robein@ymail.com>
2022-02-14 17:48:06 -0600

    Added locations to the maps

--- main.py
+++ main.py
@@ -13,10 +13,8 @@ from starlette.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from os.path import exists as file_exists
 from pykml import parser
-import kml2geojson
 
-import networkx as nx
-from pyvis.network import Network
+
 plt.rcParams['figure.figsize'] = [12, 12]
 
 app = FastAPI()
@@ -26,10 +24,16 @@ app.mount("/static", StaticFiles(directory="assignment2"), name="static")
 async def read_index():
     return FileResponse('index.html')
 
-@app.get("/map/abila")
-async def read_map():
+@app.get("/locations")
+async def get_locations():
 
-	kml2geojson.main.convert('assignment2/Geospatial/Abila.kml', '')
+	return [{"name":"Gyro's place","lat":36.06921504029343, "long":24.86214637756348},
+	{"name":"Coffee Shop","lat":36.07961504029343, "long":24.86734657756348},
+	{"name":"GasTech HQ","lat":36.07978504029343, "long":24.83734657756348},
+	{"name":"Temporary","lat":36.07161504029343, "long":24.83734657756348},
+	{"name":"Abila Airport","lat":36.04978504029343, "long":24.89734657756348}]
+
+	
     
 
 
